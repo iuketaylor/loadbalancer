35c3e98300be6b2605a07f5e5ba8cc33208257aa
Luke Taylor <luke.taylor@bbc.co.uk>
2025-06-20 11:28:47 +0100

    implement round-robin

---
+++ cmd/backend/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var port = flag.String("port", "8081", "Server port")
+var serverId = flag.String("id", "1", "Server Id")
+
+func handleHome(w http.ResponseWriter, req *http.Request) {
+	log.Printf("Replied with a hello message")
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello From Backend Server %s", *serverId)
+}
+
+func main() {
+	flag.Parse()
+
+	log.Printf("Backend server %s listening on port %s...", *serverId, *port)
+	http.HandleFunc("/", handleHome)
+	http.ListenAndServe(":"+*port, nil)
+}
